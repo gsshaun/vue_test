176847abc25a92d70d77933e6680137b0f1a442b
Golam <sarwar@linkstaff.co.jp>
2021-04-20 14:00:12 +0900

    Initated

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <button v-on:click="clickMe">Show</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    // HelloWorld
+  }, methods: {
+    clickMe() 
+    {
+      alert("You Did");
+    }
   }
-}
+};
 </script>
 
 <style>
