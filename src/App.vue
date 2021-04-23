e38ac11f1d7969f5d321a18df95d9d7d2bbf4de3
Golam <sarwar@linkstaff.co.jp>
2021-04-23 10:27:01 +0900

    Updated

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <button v-on:click="clickMe">Show</button>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
-// import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios";
 export default {
   name: "App",
-  components: {
-    // HelloWorld
-  }, methods: {
-    clickMe() 
-    {
-      axios.get("https://api.npms.io/v2/search?q=vue")
-    .then(response => {alert(response.data.total)
-    console.log(response.data)});
-    }
-  }
+  components: {},
+  methods: {
+    clickMe() {
+      axios.get("https://api.npms.io/v2/search?q=vue").then((response) => {
+        alert("Response From AXIOS GET: " + response.data.total);
+        console.log("Response From AXIOS GET: ", response.data);
+      });
+    },
+  },
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
